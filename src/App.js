911b815a7803a4b71058189a98d9915a36d85c60
jm <jmin6214@gmail.com>
2023-07-18 00:02:35 +0900

    스터디 과제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <nav className="nav">
+        <div className="container">
+          <h1 className="nav-logo">
+            <a href="./index.html">JUICY</a>
+          </h1>
+          <div className="nav-menu">
+            <ul className="nav-btn">
+              <li>HOME</li>
+              <li>MENU</li>
+              <li>INSTA</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header className="hd">
+        <div className="container">
+          <h2>DO WANNA SWEET?</h2>
+          <p>THE ANSWER IS JUICY</p>
+        </div>
       </header>
+
+      <section className="sec">
+        <div className="container">
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+        </div>
+        <div className="container">
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+        </div>
+        <div className="container">
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+          <article className="atc">
+            <div className="card-img">
+              <img src="./이미지2.jpg" alt="이미지2" />
+              <p>JUICY<br />5,000won</p>
+            </div>
+          </article>
+        </div>
+      </section>
+
+      <footer className="ft">
+        <div className="container">
+          <p>&copy;JUICY IS ALWAYS SWEEEET!</p>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
